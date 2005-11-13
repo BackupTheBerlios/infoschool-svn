2d5ef25bb3a7e29885b87aaed0ac0ed4ec0d23ab
maikel <maikel@6459c649-2afb-0310-b061-a1af72efd182>
2005-11-13 01:10:00 +0000

    queries to create new tables

--- branches/dateiaustausch/setup/update/func.php
+++ branches/dateiaustausch/setup/update/func.php
@@ -8,6 +8,7 @@
   global $db;
   $query = 'create table filesystem (id bigint unsigned primary key auto_increment, rel_to bigint unsigned not null, filetype varchar(32) not null, owner smallint(5) unsigned not null, last_change datetime not null, name varchar(64) not null, data blob not null)';
   $query = 'create table filesystem_rights_person (id bigint unsigned auto_increment primary key, fs_id bigint unsigned not null, person_id smallint(5) unsigned not null, rights tinyint unsigned not null, unique (fs_id, person_id))';
+  $query = 'create table filesystem_rights_group (id bigint unsigned auto_increment primary key, fs_id bigint unsigned not null, group_id smallint(5) unsigned not null, rights tinyint unsigned not null, unique (fs_id, group_id))';
  }
  
  function update_2005_11_07_18_23() {
