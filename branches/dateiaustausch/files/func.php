6847a0eb425c8093a6e51e4fd6110ba12c5a9133
maikel <maikel@6459c649-2afb-0310-b061-a1af72efd182>
2006-08-30 11:48:50 +0000

    files: upload size from config and visible

--- branches/dateiaustausch/files/func.php
+++ branches/dateiaustausch/files/func.php
@@ -1,9 +1,22 @@
 <?php
 /*
  * This file is part of Infoschool - a web based school intranet.
- * Copyright (C) 2005 Maikel Linke
+ * Copyright (C) 2006 Maikel Linke
  */
  
+ /* Selects the minimum value of an array */
+ function array_min($arr) {
+  $min = reset($arr);
+  while ($v = next($arr)) {
+   if ($v < $min) $min = $v;
+  }
+  return $min;
+ }
+ 
+ /*
+  * Reads a (Mega, Kilo) byte value from php.ini
+  * and conerts it to absolute bytes (base 2).
+  */
  function ini_get_bytes($name) {
   $ini_val = ini_get($name);
   $int = substr($ini_val,0,-1);
@@ -15,11 +28,17 @@
   return $val;
  }
  
+ /*
+  * Reads some limits from the config
+  * and selects the minimum.
+  */
  function get_maximum_upload_size() {
-  $db_max = pow(2,24) - 1; // 16 MB
-  $script_max = ini_get_bytes('memory_limit');
-  $post_max = ini_get_bytes('post_max_size');
-  $upload_max = ini_get_bytes('upload_max_filesize');
-  return pow(2,21);
+  $limits['database'] = pow(2,32) - 1; // 4 GB
+  /* we trust the sysadmin :)
+  ini_get_bytes('memory_limit');
+  ini_get_bytes('post_max_size');
+  */
+  $limits['upload'] = ini_get_bytes('upload_max_filesize');
+  return array_min($limits);
  }
 ?>
--- branches/dateiaustausch/files/var.php
+++ branches/dateiaustausch/files/var.php
@@ -3,16 +3,6 @@
  * This file is part of Infoschool - a web based school intranet.
  * Copyright (C) 2006 Maikel Linke
  */
-/* rights
- 1 1	read
- 2 2	upload / make directory
- 3 4	rename own
- 4 8	delete own
- 5 16	change rights of own
- 6 32	rename
- 7 64	delete
- 8 128	change rights
-*/
 /* 
  * Files are stored in a mysql database.
  * This sets a limit of the filesize
@@ -20,8 +10,19 @@
  *
  * In most cases the filesize will be
  * limited by the http-server to 1-16 MB.
+ * But there are some more potential limits.
  * The definite value is stored in
  * $max_upload_size.
+ */
+/* rights (bit code)
+ * 1 1		read
+ * 2 2		upload / make directory
+ * 3 4		rename own
+ * 4 8		delete own
+ * 5 16		change rights of own
+ * 6 32		rename
+ * 7 64		delete
+ * 8 128	change rights
  */
  include 'func.php';
  include_once 'class.php';
