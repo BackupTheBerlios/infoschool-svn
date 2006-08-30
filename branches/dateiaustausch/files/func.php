8dd6872aba02443a89fbacf6814fca384dee17c6
maikel <maikel@6459c649-2afb-0310-b061-a1af72efd182>
2006-08-30 13:42:04 +0000

    get_maximum_upload_size() updated

--- branches/dateiaustausch/files/func.php
+++ branches/dateiaustausch/files/func.php
@@ -13,19 +13,21 @@
   return $min;
  }
  
- /*
-  * Reads a (Mega, Kilo) byte value from php.ini
-  * and conerts it to absolute bytes (base 2).
-  */
- function ini_get_bytes($name) {
-  $ini_val = ini_get($name);
-  $int = substr($ini_val,0,-1);
-  $exp_char = substr($ini_val,-1);
-  $exp = 0;
-  if ($exp_char == 'K') $exp = 10;
-  if ($exp_char == 'M') $exp = 20;
-  $val = $int * pow(2,$exp);
+ /* Converts shorthand notation to absolute bytes */
+ function short2bytes($val) {
+  $val = trim($val);
+  $last = strtolower($val{strlen($val)-1});
+  switch ($last) {
+   case 'g': $val*= 1024;
+   case 'm': $val*= 1024;
+   case 'k': $val*= 1024;
+  }
   return $val;
+ }
+ 
+ /* Reads a byte value from php.ini */
+ function ini_get_bytes($name) {
+  return short2bytes(ini_get($name));
  }
  
  /*
