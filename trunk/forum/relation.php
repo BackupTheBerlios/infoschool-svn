c39870e5985df725e8bb43a8371c5af321afc33a
maikel <maikel@6459c649-2afb-0310-b061-a1af72efd182>
2006-11-05 19:44:42 +0000

    forum: relation_build skript

---
+++ trunk/forum/relation.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * This file is part of Infoschool - a web based school intranet.
+ * Copyright (C) 2006 Maikel Linke
+ */
+ include 'var.php';
+
+ $output->secure();
+ 
+ class entry_relation extends entry {
+ 
+  function new_entry() {
+   return new entry_relation();
+  }
+  
+  function relation_delete() {
+   $this->db->query('delete from forum_relation');
+  }
+  
+  function relation_build() {
+   $id = $this->data['id'];
+   $parent = &$this;
+   $level = 0;
+   while (isset($parent) && $parent->data['id']) {
+    $rel_to = $parent->data['rel_to'];
+    $level++;
+    $this->db->query('insert into forum_relation values ("'.$rel_to.'","'.$id.'","'.$level.'")');
+    $parent = &$this->entries[$rel_to];
+   }
+   $this->load_answers();
+   foreach ($this->answers as $id => $answer) {
+    $answer->relation_build();
+   }
+  }
+  
+  function load_answers() {
+   $query = 'select
+              forum.id,
+              forum.rel_to
+             from forum
+             where forum.rel_to="'.$this->data['id'].'"';
+   $result = $this->db->query($query);
+   while ($data = mysql_fetch_array($result)) {
+    $id = $data['id'];
+    $answer = &$this->entries[$id];
+    if (!isset($answer)) {
+     $answer = $this->new_entry();
+     $answer->set_data($data);
+     $this->answers[$id] = &$answer;
+     $answer->set_parent($this);
+    }
+   }
+  }
+  
+  function set_parent($parent) {
+   $this->entries = &$parent->entries;
+  }
+  
+ }
+ 
+ $entry = new entry_relation();
+ $entry->id = 0;
+ $entry->load();
+ $entry->relation_delete();
+ $entry->relation_build();
+ 
+?>
