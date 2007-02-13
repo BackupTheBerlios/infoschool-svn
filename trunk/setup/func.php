f99d811f37408555ef40558ffb0a3fbee1da4a9a
maikel <maikel@6459c649-2afb-0310-b061-a1af72efd182>
2007-02-13 14:25:22 +0000

    old setup

--- trunk/setup/func.php
+++ trunk/setup/func.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * This file is part of Infoschool - a web based school intranet.
- * Copyright (C) 2007 Maikel Linke
+ * Copyright (C) 2004 Maikel Linke
  */
 
  // creates a random string with certain length
@@ -16,32 +16,6 @@
    $s.= $char[rand(0,sizeof($char)-1)];
   }
   return $s;
- }
- 
- // writes config files
- function mysql_config() {
- 	if (!isset($_POST['user_name'])) return false;
- 	if (!isset($_POST['user_passwd'])) return false;
- 	if (!isset($_POST['user_passwd2'])) return false;
- 	if (!isset($_POST['db'])) return false;
-	if ($_POST['user_passwd'] != $_POST['user_passwd2']) return false;
-	global $root;
-  	$mysql_data['host'] = 'localhost';
-  	$mysql_data['user'] = $_POST['user_name'];
-  	$mysql_data['passwd'] = $_POST['user_passwd'];
-  	$mysql_data['db'] = $_POST['db'];
-  	$mysql_cfg = new tmpl('mysql.php',$mysql_data);
-  	$mysql_cfg->data = $mysql_cfg->fdata;
-  	$mysql_cfg->path = $root.'.htsecret/etc/';
-  	$mysql_cfg->write();
-  	$mysql_cfg->chmod(0600);
-  	$random = md5(random_string());
-  	$random_pw = new tmpl('random.php',array('random'=>$random));
-  	$random_pw->data = $random_pw->fdata;
-  	$random_pw->path = $root.'.htsecret/etc/';
-  	$random_pw->write();
-  	$random_pw->chmod(0600);
-	return true;
  }
 
 ?>
