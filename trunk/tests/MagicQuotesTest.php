a6174fa30e740284b9dba270018fe6f2a3410d90
maikel <maikel@6459c649-2afb-0310-b061-a1af72efd182>
2007-10-21 21:11:41 +0000

    MagicQuotes tested

--- trunk/tests/MagicQuotesTest.php
+++ trunk/tests/MagicQuotesTest.php
@@ -4,16 +4,55 @@
  * Copyright (C) 2007 Maikel Linke
  */
 
-require_once 'PHPUnit.php';
+require_once 'PHPUnit/Framework.php';
 
 require_once '../class_MagicQuotes.php';
 
-class MagicQuotesTest extends PHPUnit_TestCase {
+class MagicQuotesTest extends PHPUnit_Framework_TestCase {
+	
+	protected $testString;
+	protected $testStringQuoted;
+	
+	public function MagicQuotesTest() {
+		$this->testString = '"Hello\ world!\'\'';
+		$this->testStringQuoted = addslashes($this->testString);
+	}
+	
+	protected function setUp() {
+		if (get_magic_quotes_gpc()) {
+			$_GET['test'] = $this->testStringQuoted;
+		}
+		else {
+			$_GET['test'] = $this->testString;
+		}
+	}
+	
+	protected function tearDown() {
+		unset($_GET['test']);
+	}
 
 	function test_turnOn() {
+		$mq = new MagicQuotes();
+		$mq->turnOn();
+		$this->assertEquals($this->testStringQuoted,$_GET['test']);
+		$mq->turnOn();
+		$mq->turnOff();
+		$mq->turnOn();
+		$mq->turnOn();
+		$this->assertEquals($this->testStringQuoted,$_GET['test']);
 	}
 
 	function test_turnOff() {
+		$mq = new MagicQuotes();
+		$mq->turnOff();
+		$this->assertEquals($this->testString,$_GET['test']);
+		$mq->turnOff();
+		$mq->turnOff();
+		$mq->turnOff();
+		$this->assertEquals($this->testString,$_GET['test']);
+		$mq->turnOn();
+		$mq->turnOff();
+		$this->assertEquals($this->testString,$_GET['test']);
 	}
 
 }
