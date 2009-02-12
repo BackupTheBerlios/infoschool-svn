5ae078985ca41d12f23ba9268c447b1efd2515d9
maikel <maikel@6459c649-2afb-0310-b061-a1af72efd182>
2009-02-12 21:58:00 +0000

    mail form: name and mail interchanged to protect from spam

--- trunk/dokumentation/mail.php
+++ trunk/dokumentation/mail.php
@@ -1,12 +1,18 @@
 <?php
- include 'var.php';
+include 'var.php';
 
- if($_POST['text']){
-  $header = 'From: ';
-  if($_POST['name']) $header.= $_POST['name'];
-  if($_POST['mail']) $header.= ' <'.$_POST['mail'].'>';
-  mail('infoschool','[Infoschool] Nachricht an die Entwickler',stripslashes($_POST['text']),$header);
- }
+if($_POST['text']){
+	/*
+	 * Attention!
+	 * The fields mail and name are interchanged. Actually this keeps a lot of spam away.
+	 */
+	if (!strstr($_POST['mail'],'@')) {
+		$header = 'From: ';
+		if($_POST['mail']) $header.= $_POST['mail'];
+		if($_POST['name']) $header.= ' <'.$_POST['name'].'>';
+		mail('infoschool','[Infoschool] Nachricht an die Entwickler',stripslashes($_POST['text']),$header);
+	}
+}
 
- redirect('./');
+redirect('./');
 ?>
